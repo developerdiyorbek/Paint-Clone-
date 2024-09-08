b64f4bd317a221374bbda3cdf29721e414d53b04
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-09-08 19:21:49 +0500

    draw rectangle and fill shape

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
+// global variables
 const canvas = document.querySelector("canvas");
+const toolBtns = document.querySelectorAll(".tool");
+const fillColor = document.querySelector("#fill-color");
 
+// variables
 let ctx = canvas.getContext("2d");
 let isDrawing = false;
 let brushWidth = 5;
+let selectedTool = "brush";
+let prevMouseX;
+let prevMouseY;
+let snapshot;
 
+// set to canvas width and height
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 });
 
 // start drawing
-const startDraw = () => {
+const startDraw = (e) => {
   isDrawing = true;
+  prevMouseX = e.offsetX;
+  prevMouseY = e.offsetY;
   ctx.beginPath();
   ctx.lineWidth = brushWidth;
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
 // stop drawing
@@ -21,12 +33,54 @@ const stopDraw = () => {
   isDrawing = false;
 };
 
-const drawing = (e) => {
-  if (!isDrawing) return;
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
+// draw rectangle
+const drawRectangle = (e) => {
+  if (!fillColor.checked) {
+    return ctx.strokeRect(
+      e.offsetX,
+      e.offsetY,
+      prevMouseX - e.offsetX,
+      prevMouseY - e.offsetY
+    );
+  }
+  return ctx.fillRect(
+    e.offsetX,
+    e.offsetY,
+    prevMouseX - e.offsetX,
+    prevMouseY - e.offsetY
+  );
 };
 
+// drawing
+const drawing = (e) => {
+  if (!isDrawing) return;
+  ctx.putImageData(snapshot, 0, 0);
+
+  switch (selectedTool) {
+    case "brush":
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+      break;
+    case "rectangle":
+      drawRectangle(e);
+      break;
+
+    default:
+      break;
+  }
+};
+
+// tool btns
+toolBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".options .active").classList.remove("active");
+    btn.classList.add("active");
+    selectedTool = btn.id;
+    console.log(selectedTool, "selected tool");
+  });
+});
+
+// event listener
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mousemove", drawing);
 canvas.addEventListener("mouseup", stopDraw);
