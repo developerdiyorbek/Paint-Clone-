91e9d128c57d460d77adb0716b66793e944d5659
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-09-11 23:08:13 +0500

    draw triangle

--- main.js
+++ main.js
@@ -61,6 +61,16 @@ const drawCircle = (e) => {
   fillColor.checked ? ctx.fill() : ctx.stroke();
 };
 
+//draw triangle
+const drawTriangle = (e) => {
+  ctx.beginPath();
+  ctx.moveTo(prevMouseX, prevMouseY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+  ctx.closePath();
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
 // drawing
 const drawing = (e) => {
   if (!isDrawing) return;
@@ -76,6 +86,9 @@ const drawing = (e) => {
       break;
     case "circle":
       drawCircle(e);
+      break;
+    case "triangle":
+      drawTriangle(e);
       break;
 
     default:
